63cf66b740f51b2f05799441ab204d9834fc7e56
Hinstein <1191915168@qq.com>
2019-01-09 16:38:14 +0800

    2019.1.9

--- src/main/java/com/hinstein/blog/Controller/SettingController.java
+++ src/main/java/com/hinstein/blog/Controller/SettingController.java
@@ -4,7 +4,6 @@ import com.hinstein.blog.bean.Admin;
 import com.hinstein.blog.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
@@ -25,30 +24,26 @@ public class SettingController {
     AdminService adminService;
 
     @GetMapping("/admin/settings")
-    public String setting()
-    {
+    public String setting() {
         return "admin/settings/setting";
     }
 
     @ResponseBody
     @PostMapping("/admin/setting/changePassword")
-    public Map<String,String> changePassword(@RequestParam("newPassword") String newPassword,
-                                             @RequestParam("oldPassword") String oldPassword,
-                                             HttpSession session){
-        Map<String,String> map = new HashMap<>();
-        String username= (String)session.getAttribute("loginUser");
+    public Map<String, String> changePassword(@RequestParam("newPassword") String newPassword,
+                                              @RequestParam("oldPassword") String oldPassword,
+                                              HttpSession session) {
+        Map<String, String> map = new HashMap<>();
+        String username = (String) session.getAttribute("loginUser");
         Admin admin = adminService.findByUserName(username);
-        if(admin.getPassword().equals(oldPassword)){
+        if (admin.getPassword().equals(oldPassword)) {
             admin.setPassword(newPassword);
-            map.put("msg","更新密码成功！");
+            map.put("msg", "更新密码成功！");
             adminService.changePassword(admin);
-        }
-        else if(newPassword.equals("")||oldPassword.equals(""))
-        {
-            map.put("msg","密码不能为空");
-        }
-        else{
-            map.put("msg","旧密码错误");
+        } else if (newPassword.equals("") || oldPassword.equals("")) {
+            map.put("msg", "密码不能为空");
+        } else {
+            map.put("msg", "旧密码错误");
         }
         return map;
     }
